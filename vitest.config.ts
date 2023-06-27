12912c6dda15791df49645ba262ca8cab095dedd
Etienne Deladonchamps <etienne@husprey.com>
2023-06-27 14:04:12 +0200

    Update setup

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  test: {
-    setupFiles: './tests/globalSetup.ts',
-  },
+  plugins: [],
+  test: { setupFiles: './tests/globalSetup.ts' },
 });
